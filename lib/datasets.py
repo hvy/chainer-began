b2b18258670fc3ccd6c3207dc206f295f31a4b90
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2017-04-14 02:44:12 +0000

    Crop CelebA images

--- lib/datasets.py
+++ lib/datasets.py
@@ -6,9 +6,10 @@ from PIL import Image
 
 
 class ImageDataset(chainer.dataset.DatasetMixin):
-    def __init__(self, paths, size):
+    def __init__(self, paths, size=None, crop=None):
         self.paths = paths
         self.size = size
+        self.crop = crop
 
     def __len__(self):
         return len(self.paths)
@@ -17,7 +18,10 @@ class ImageDataset(chainer.dataset.DatasetMixin):
         path = self.paths[i]
 
         with Image.open(path) as f:
-            f = f.resize(self.size, Image.ANTIALIAS)
+            if self.crop is not None:
+                f = f.crop(self.crop)
+            if self.size is not None:
+                f = f.resize(self.size, Image.ANTIALIAS)
             im = np.asarray(f, dtype=np.float32)
 
         # NOTE: Uncomment if it is necessary to modify the image
@@ -25,13 +29,13 @@ class ImageDataset(chainer.dataset.DatasetMixin):
 
         im = im.transpose((2, 0, 1))
 
-        # Rescale to [-1, 1]
+        # Rescale from [0, 255] to [-1, 1]
         im *= (2 / 255)
         im -= 1
 
         return im
 
 
-def get_celeba(root, size=(64, 64)):
+def get_celeba(root, size=(64, 64), crop=(25, 50, 25+128, 50+128)):
     paths = glob.glob('{}/Img/img_align_celeba_png/*.png' .format(root))
-    return ImageDataset(paths, size)
+    return ImageDataset(paths, size, crop)
